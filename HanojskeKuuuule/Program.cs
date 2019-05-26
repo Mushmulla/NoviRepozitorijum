ccba5c414fbfbbf5920c1c6bc877576179d634c0
Mushmulla <50882009+Mushmulla@users.noreply.github.com>
2019-05-26 11:48:20 +0200

    Pokusaj za Hanojske kule

--- HanojskeKuuuule/Program.cs
+++ HanojskeKuuuule/Program.cs
@@ -8,8 +8,36 @@ namespace HanojskeKuuuule
 {
     class Program
     {
-        static void Main(string[] args)
+        class HanojskeKule
         {
+            public void SrediPrstenove(int brojPrstenova, char Source, char Tmp, char Destination)
+            {
+                if (brojPrstenova < 1)
+                {
+                    return;
+                }
+                if (brojPrstenova == 1)
+                {
+                    Console.WriteLine($"Prebacuje se prsten {brojPrstenova} sa {Source} na {Destination}");
+                    return;
+                }
+                SrediPrstenove(brojPrstenova - 1, Source, Destination, Tmp);
+                Console.WriteLine($"Prebacuje se prsten {brojPrstenova} sa {Source} na {Destination}");
+                SrediPrstenove(brojPrstenova - 1, Tmp, Source, Destination);
+
+
+
+            }
+
+
+             static void Main(string[] pera)
+             {
+                HanojskeKule hk = new HanojskeKule();
+                hk.SrediPrstenove(16, 'a', 'b', 'c');
+
+                Console.ReadLine();
+             }
         }
     }
+    
 }
